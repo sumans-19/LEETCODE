79066dbbd2e5e8c0ad06e9bb0e37823d79c90485
Suman S <147817337+sumans-19@users.noreply.github.com>
2024-12-14 11:49:46 +0530

    Update and rename ARRAYS.4 to ARRAYS.4.java

--- ARRAYS.4.java
+++ ARRAYS.4.java
@@ -1,5 +1,5 @@
-10} Given an array nums of size n, return the majority element.
-The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+// 10} Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
 
 CODE=>
