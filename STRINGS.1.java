9a7df84fb75e086f378243a25d49c3b48744b593
Suman S <147817337+sumans-19@users.noreply.github.com>
2024-12-16 21:18:12 +0530

    Update STRINGS.1.java

--- STRINGS.1.java
+++ STRINGS.1.java
@@ -56,3 +56,19 @@ bool isValid(char* s) {
     }  
     return top == -1; 
 }
+
+
+
+// 3}Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal 
+// substring
+//  consisting of non-space characters only.
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        String[] words=s.split(" ");
+        return words[words.length-1].length();
+    }
+}
