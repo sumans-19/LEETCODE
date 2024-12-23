4c9252f654e080475de116063349204999fcd1b9
Suman S <147817337+sumans-19@users.noreply.github.com>
2024-12-23 22:11:06 +0530

    Update DYNAMIC.java

--- DYNAMIC.java
+++ DYNAMIC.java
@@ -25,3 +25,37 @@ CODE=>
 // Time complexity: O(n)
 // Space complexity: O(1)
 // This solution efficiently calculates the number of ways to climb n stairs using dynamic programming principles.
+
+
+
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+// You may assume that you have an infinite number of each kind of coin.
+
+CODE =>
+  class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] minCoins = new int[amount + 1];
+        Arrays.fill(minCoins, amount + 1);
+        minCoins[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    minCoins[i] = Math.min(minCoins[i], 1 + minCoins[i - coins[j]]);
+                }
+            }
+        }
+
+        return minCoins[amount] != amount + 1 ? minCoins[amount] : -1;        
+    }
+}
+
+
+// Complexity:
+// Time Complexity: 
+// 𝑂(amount × coins.length)
+// O(amount×coins.length), since we iterate over all amounts and all coins.
+// Space Complexity: 
+// 𝑂(amount)
+// O(amount), as we use a single array of size amount + 1.
