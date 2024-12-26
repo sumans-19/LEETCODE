b33cc39548255d0f188ceca27e073fccf360a9dc
Suman S <147817337+sumans-19@users.noreply.github.com>
2024-12-26 23:13:19 +0530

    Update DYNAMIC02.java

--- DYNAMIC02.java
+++ DYNAMIC02.java
@@ -35,3 +35,59 @@ class GFG {
     }
 }
 
+
+
+// DYNAMIC PROGRAMINNG CAN BE USED TO REDUCE THE SAME CODES TIME COMPLEXITY AND SPACE COMPLEXITY 
+
+// A space optimized
+// based Java program
+// to print first n 
+// Tribinocci numbers.
+import java.io.*;
+
+class GFG {
+    
+    static void printTrib(int n)
+    {
+        if (n < 1)
+            return;
+    
+        // Initialize first
+        // three numbers
+        int first = 0, second = 0;
+        int third = 1;
+    
+        System.out.print(first + " ");
+        if (n > 1)
+            System.out.print(second + " ");
+        
+        if (n > 2)
+            System.out.print(second + " ");
+    
+        // Loop to add previous
+        // three numbers for
+        // each number starting
+        // from 3 and then assign
+        // first, second, third
+        // to second, third, and curr
+        // to third respectively
+        for (int i = 3; i < n; i++) 
+        {
+            int curr = first + second + third;
+            first = second;
+            second = third;
+            third = curr;
+    
+            System.out.print(curr +" ");
+        }
+    }
+    
+    // Driver code
+    public static void main(String args[])
+    {
+        int n = 10;
+        printTrib(n);
+    }
+}
+
+
