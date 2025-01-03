bc0c1a390b8e148781204ccd688415f31347c8ce
Suman S <147817337+sumans-19@users.noreply.github.com>
2025-01-03 20:45:31 +0530

    Update MATH.01.java

--- MATH.01.java
+++ MATH.01.java
@@ -80,3 +80,29 @@ class Solution {
 
 Time Complexity: ( O(n) ), as we iterate from 1 to ( n ).
 Space Complexity: ( O(n) ), as we store the result in a list.
+
+
+
+
+// You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+CODE =>
+  class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            int temp = first + second;
+            first = second;
+            second = temp;
+        }
+        
+        return second;
+    }
+}
+
