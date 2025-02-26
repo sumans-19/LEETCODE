1340831bcbf41b2cebdc4043d6023828017a5d85
Suman S <147817337+sumans-19@users.noreply.github.com>
2025-02-26 22:33:46 +0530

    Update ARRAYS.4.java

--- ARRAYS.4.java
+++ ARRAYS.4.java
@@ -14,4 +14,44 @@ CODE=>
     }
 }
 
-11} 
+// 11} Sort list
+//  Given the head of a linked list, return the list after sorting it in ascending order.
+
+CODE=>
+  /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        int len = 0;
+        ListNode temp = head;
+        while(temp != null){
+            temp = temp.next;
+            len++;
+        }
+        int[] listarray = new int[len];
+        int i = 0;
+        ListNode newtemp = head;
+        while(newtemp != null){
+            listarray[i] = newtemp.val;
+            i++;
+            newtemp = newtemp.next;
+        }
+        Arrays.sort(listarray);
+        ListNode temp2 = head;
+        int j = 0;
+        while(temp2 != null){
+            temp2.val = listarray[j];
+            j++;
+            temp2 = temp2.next;
+        }
+        return head;
+    }
+}
