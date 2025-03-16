c317879ae79ace0b00f5976bd42433c86bdf842d
Suman S <147817337+sumans-19@users.noreply.github.com>
2025-03-16 16:31:52 +0530

    Create Leet01.java

---
+++ DSA-Mode/Leet01.java
@@ -0,0 +1,23 @@
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] 
+// for each 0 <= i < nums.length and return it.
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+
+public class Leet01 {
+    public int[] buildArray(int[] nums) {
+        int l = nums.length;
+        int[] ans = new int[l];
+        for(int i = 0; i < l; i++) {
+            ans[i] = nums[nums[i]];
+            }
+            return ans;
+    }
+    public static void main(String[] args) {
+        Leet01 leet01 = new Leet01();
+        int[] nums = {0,2,1,5,3,4};
+        int ans[] = leet01.buildArray(nums);
+        for(int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+            }
+    }
+}
