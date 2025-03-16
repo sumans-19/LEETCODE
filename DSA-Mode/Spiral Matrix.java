dc572cdab1055e53ae4bef7f96e225f334f4252d
Suman S <147817337+sumans-19@users.noreply.github.com>
2025-03-16 18:27:11 +0530

    Create Spiral Matrix.java

---
+++ DSA-Mode/Spiral Matrix.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return res;
+        }
+        
+        int rowBegin = 0, rowEnd = matrix.length - 1;
+        int colBegin = 0, colEnd = matrix[0].length - 1;
+        
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            for (int i = colBegin; i <= colEnd; i++) {
+                res.add(matrix[rowBegin][i]);
+            }
+            rowBegin++;
+
+            for (int i = rowBegin; i <= rowEnd; i++) {
+                res.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+
+            if (rowBegin <= rowEnd) {
+                for (int i = colEnd; i >= colBegin; i--) {
+                    res.add(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+
+            if (colBegin <= colEnd) {
+                for (int i = rowEnd; i >= rowBegin; i--) {
+                    res.add(matrix[i][colBegin]);
+                }
+                colBegin++;
+            }
+        }
+        
+        return res;
+    }
+    public static void main(String[] args) {
+       SpiralMatrix leet02 = new SpiralMatrix();
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+            };
+            List<Integer> res = leet02.spiralOrder(matrix);
+            System.out.println(res);
+    }
+}
