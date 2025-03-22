64635de63a6fe3de0a1dfbcbeb98e9e8be0e74f8
Suman S <147817337+sumans-19@users.noreply.github.com>
2025-03-22 18:13:27 +0530

    Create floor of a number.java

---
+++ DSA-Mode/floor of a number.java
@@ -0,0 +1,32 @@
+public class code07 {
+    public static void main(String[] args) {         
+        int[] arr = {2,3,5,9,14,16,18};         
+        int target = -1;         
+        int ans = search(arr, target);         
+        System.out.println(ans);      
+    }     
+
+    public static int search(int[] nums, int target) {         
+        int len = nums.length;         
+        int start = 0;         
+        int end = len - 1;          
+
+        while (start <= end) {              
+            int mid = start + (end - start) / 2;              
+            if (target < nums[mid]) {                 
+                end = mid - 1;              
+            } else if (target > nums[mid]) {                 
+                start = mid + 1;              
+            } else {             
+                return nums[mid];              
+            }          
+        }         
+
+        // Edge case: If target is smaller than the smallest element, return -1
+        if (end < 0) {
+            return -1;  
+        }
+
+        return nums[end];  // Return the floor element
+    }
+}
